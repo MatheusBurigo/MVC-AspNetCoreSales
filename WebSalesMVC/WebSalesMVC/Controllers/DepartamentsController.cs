dc9479ef94e5c6bb0683391ea3e05f0080ebe774
Matheus Felipe Burigo <67557518+MatheusBurigo@users.noreply.github.com>
2023-01-21 01:38:38 -0300

    First Model Controller View - Department

---
+++ WebSalesMVC/WebSalesMVC/Controllers/DepartamentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebSalesMVC.Models;
+
+namespace WebSalesMVC.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departaments> list = new List<Departaments>();
+            list.Add(new Departaments {Id = 1, Name = "Eletronics" });
+            list.Add(new Departaments { Id = 2, Name = "Fashion" });
+
+            //Here i will sent my Departament List to my View()
+            return View(list);
+        }
+    }
+}
--- WebSalesMVC/WebSalesMVC/Controllers/HomeController.cs
+++ WebSalesMVC/WebSalesMVC/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using WebSalesMVC.Models;
+using WebSalesMVC.Models.ViewModels;
 
 namespace WebSalesMVC.Controllers
 {
